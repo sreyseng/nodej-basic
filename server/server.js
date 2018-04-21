ed2e82a4e815ed944a02a9b9d97786f03da865ce
Srey Seng <sreyseng@gmail.com>
2018-04-21 00:42:56 -0700

    Add Post endpoint

--- server/server.js
+++ server/server.js
@@ -7,7 +7,19 @@ var { User } = require('./models/user.js');
 
 var app = express();
 
+app.use(bodyParser.json());
 
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
 
 app.listen(3000, () => {
     console.log('started on port 3000');
