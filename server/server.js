ab7535d9a8af64be99694da04875e949e4d83753
Srey Seng <sreyseng@gmail.com>
2018-04-22 12:35:06 -0700

    Set up different env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 const express = require('express');
@@ -8,7 +10,7 @@ const { Todo } = require('./models/todo.js');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
