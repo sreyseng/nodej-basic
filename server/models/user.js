8842e0222fac5552852c4a413b2f45c0a8133e8a
Srey Seng <sreyseng@gmail.com>
2018-04-22 20:37:03 -0700

    Add about endpoint + authentication 'middleware'

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,23 @@ UserSchema.methods.generateAuthToken = function generateTokenCustomHelper() {
   return user.save().then(() => token);
 };
 
+UserSchema.statics.findByToken = function findByTokenCustomHelper(token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'supersecrettoken');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 
@@ -42,10 +43,15 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user);
+    res.header('x-auth', token).send({ user });
   }).catch((e) => {
     res.status(400).send({ error: 'Unable to save user.', stackTrace: e });
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  const { user } = req;
+  res.send({ user });
 });
 
 app.get('/todos', (req, res) => {
